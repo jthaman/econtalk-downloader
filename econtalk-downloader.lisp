c4ac5cff7d8927d6fd4b1071f5c8c83e71a1d477
John Haman <mail@johnhaman.org>
2021-12-28 10:24:41 -0500

    prototype

--- econtalk-downloader.lisp
+++ econtalk-downloader.lisp
@@ -1,16 +1,9 @@
 (ql:quickload '("dexador" "lquery" "lparallel" "cxml" "cl-ppcre" "trivial-download"))
 
-(defparameter download-dir "/home/john/Downloads/")
+(defparameter *download-dir* #P"/home/john/Downloads/")
 
 (defparameter *rss-url* "https://feeds.simplecast.com/wgl4xEgL")
 (defparameter *request* (dex:get *rss-url*))
-
-;; Write the downloaded xml feed to a file.
-;; (with-open-file (s "feed.xml" :direction :output)
-;;   (write-string *request* s))
-
-;; (defparameter *parsed-content*
-;;   (cxml:parse-file "feed.xml" (cxml-dom:make-dom-builder)))
 
 (defparameter *mp3s*
   (cl-ppcre:all-matches-as-strings "https://.*.mp3" *request*))
@@ -27,6 +20,8 @@
          (setf (nth i *filenames*)
                (concatenate 'string (nth i *filenames*) ".mp3")))
 
-(defparameter *dat* (pairlis *filenames* *mp3s*))
-
-((trivial-download:download (nth 0 *mp3s*) "/home/john/Downloads/ep1.mp3")
+(loop for i from 0 to (- (length *filenames*) 1)
+      do
+         (unless
+             (probe-file (merge-pathnames *download-dir* (nth i *filenames*)))
+           (trivial-download:download (nth i *mp3s*) (nth i *filenames*))))
